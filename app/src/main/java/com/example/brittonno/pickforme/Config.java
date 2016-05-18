846c0f092d3aa2d69ff00833d1c68c2a1fca90a6
Britton <brittonno@nhrhs.lan>
2016-05-18 14:35:22 -0400

    First commit

---
+++ app/src/main/java/com/example/brittonno/pickforme/Config.java
@@ -0,0 +1,13 @@
+package com.example.brittonno.pickforme;
+
+/**
+ * Created by brittonno on 5/18/2016.
+ */
+public final class Config
+{
+    private Config()
+    {
+        final String YELP_API_KEY = "DqSfpOp46bwHA-LLhu6GrA";
+
+    }
+}
